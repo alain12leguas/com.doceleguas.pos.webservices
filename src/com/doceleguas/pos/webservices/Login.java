b7d976d8c67662496c149de44ffd42206ed35924
Alain Perez <alain.perez@openbravo.com>
2025-12-09 16:46:03 -0400

    Added roels and languages to Login service

--- src/com/doceleguas/pos/webservices/Login.java
+++ src/com/doceleguas/pos/webservices/Login.java
@@ -2,18 +2,29 @@ package com.doceleguas.pos.webservices;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.dal.service.OBQuery;
 import org.openbravo.erpCommon.utility.OBError;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.mobile.core.MobileDefaults;
 import org.openbravo.mobile.core.login.MobileCoreLoginHandler;
+import org.openbravo.mobile.core.login.ProfileUtils;
 import org.openbravo.model.ad.access.Role;
+import org.openbravo.model.ad.access.User;
+import org.openbravo.model.ad.system.Language;
+import org.openbravo.retail.posterminal.POSDefaults;
 
 public class Login extends MobileCoreLoginHandler {
   private static final long serialVersionUID = 1L;
@@ -29,14 +40,16 @@ public class Login extends MobileCoreLoginHandler {
       if (defaultRole == null) {
         throw new RoleDoNotExistException();
       }
-      JSONObject role = new JSONObject();
-      role.put("id", defaultRole.getId());
-      role.put("name", defaultRole.getName());
       JSONObject user = new JSONObject();
-      user.put("role", role);
       user.put("name", OBContext.getOBContext().getUser().getName());
       user.put("id", OBContext.getOBContext().getUser().getId());
+      user.put("defaultRole", getDefaultRoleJson());
+      user.put("roles", getRoles());
+      user.put("defaultLanguage", getDefaultLanguageJson());
+      user.put("languages", getLanguages());
       jsonResponse.put("user", user);
+
+      // JSONObject profilesJson = profile.exec(new JSONObject("{\"appName\":\"WebPOS\"}"));
       PrintWriter writer = res.getWriter();
       writer.print(jsonResponse.toString());
       writer.flush();
@@ -52,6 +65,64 @@ public class Login extends MobileCoreLoginHandler {
       responseWithError(res, error);
     }
 
+  }
+
+  private JSONObject getDefaultRoleJson() throws JSONException {
+    Role role = OBContext.getOBContext().getUser().getOBPOSDefaultPOSRole();
+    JSONObject response = new JSONObject();
+    response.put("id", role.getId());
+    response.put("identifier", role.getIdentifier());
+    return response;
+  }
+
+  private JSONObject getDefaultLanguageJson() throws JSONException {
+    User user = OBContext.getOBContext().getUser();
+    Language language;
+    if (user.getDefaultLanguage() != null) {
+      language = user.getDefaultLanguage();
+    } else if (user.getOBPOSDefaultPOSRole().getOBPOSDefaultPosLanguage() != null) {
+      language = user.getOBPOSDefaultPOSRole().getOBPOSDefaultPosLanguage();
+    } else {
+      language = OBDal.getInstance()
+          .createCriteria(Language.class)
+          .add(Restrictions.eq(Language.PROPERTY_LANGUAGE, "en_US"))
+          .list()
+          .get(0);
+    }
+    JSONObject response = new JSONObject();
+    response.put("id", language.getId());
+    response.put("identifier", language.getIdentifier());
+    return response;
+  }
+
+  private JSONArray getRoles() throws JSONException {
+    OCProfile profile = new OCProfile();
+    final List<Role> roles = profile.getRoles(new POSDefaults());
+    JSONArray rolesArray = new JSONArray();
+    for (Role role : roles) {
+      final JSONObject jsonRole = new JSONObject();
+      jsonRole.put("id", role.getId());
+      jsonRole.put("identifier", role.getIdentifier());
+      rolesArray.put(jsonRole);
+    }
+    return rolesArray;
+  }
+
+  private JSONArray getLanguages() throws JSONException {
+
+    final OBQuery<Language> languages = OBDal.getInstance()
+        .createQuery(Language.class, "(" + Language.PROPERTY_SYSTEMLANGUAGE + "=true or "
+            + Language.PROPERTY_BASELANGUAGE + "=true)");
+    languages.setFilterOnReadableClients(false);
+    languages.setFilterOnReadableOrganization(false);
+    JSONArray jsonArray = new JSONArray();
+    for (Language language : languages.list()) {
+      final JSONObject json = new JSONObject();
+      json.put("id", language.getId());
+      json.put("identifier", language.getIdentifier());
+      jsonArray.put(json);
+    }
+    return jsonArray;
   }
 
   private void responseWithError(HttpServletResponse res, OBError err) {
@@ -73,6 +144,14 @@ public class Login extends MobileCoreLoginHandler {
 
     public RoleDoNotExistException() {
       super("The user does not have a defined role.");
+    }
+  }
+
+  class OCProfile extends ProfileUtils {
+    @Override
+    protected List<Role> getRoles(MobileDefaults defaults) {
+      // String appName = "WebPOS";
+      return super.getRoles(defaults);
     }
   }
 }
