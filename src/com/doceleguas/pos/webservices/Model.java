c6dc47ce39a28dccfe16a3bb6e2131e6a1715d28
Alain Perez <alain.perez@openbravo.com>
2025-12-16 23:34:44 -0400

    Variante 1

---
+++ src/com/doceleguas/pos/webservices/Model.java
@@ -0,0 +1,46 @@
+package com.doceleguas.pos.webservices;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Qualifier;
+
+import org.codehaus.jettison.json.JSONObject;
+import org.openbravo.mobile.core.master.MasterDataProcessHQLQuery.MasterDataModel;
+
+public abstract class Model {
+  public abstract JSONObject exec(JSONObject jsonsent);
+
+  public abstract String getName();
+
+  @Qualifier
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target({ ElementType.TYPE, ElementType.FIELD })
+  public @interface ModelAnnotation {
+    String value();
+
+    /**
+     * Supports instantiation of the {@link MasterDataModel} qualifier. It can be used to select a
+     * master data model (MasterDataProcessHQLQuery instance) in particular.
+     */
+    @SuppressWarnings("all")
+    public static final class Literal extends AnnotationLiteral<ModelAnnotation>
+        implements ModelAnnotation {
+      private static final long serialVersionUID = 1L;
+
+      private final String value;
+
+      public Literal(String value) {
+        this.value = value;
+      }
+
+      @Override
+      public String value() {
+        return value;
+      }
+    }
+  }
+}
---
+++ src/com/doceleguas/pos/webservices/OCBusinessPartner.java
@@ -0,0 +1,21 @@
+package com.doceleguas.pos.webservices;
+
+import org.codehaus.jettison.json.JSONObject;
+
+import com.doceleguas.pos.webservices.Model.ModelAnnotation;
+
+@ModelAnnotation("OCBusinessPartner")
+public class OCBusinessPartner extends Model {
+
+  @Override
+  public JSONObject exec(JSONObject jsonsent) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public String getName() {
+    return "OCBusinessPartner";
+  }
+
+}
