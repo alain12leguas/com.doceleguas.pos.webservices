723401238c0ea161ac4b75cc1e32b156b1a19bbc
Alain Perez <alain.perez@openbravo.com>
2026-04-01 14:38:30 -0400

    Fixed Tax Zone query

--- src/com/doceleguas/pos/webservices/OCTaxZone.java
+++ src/com/doceleguas/pos/webservices/OCTaxZone.java
@@ -27,76 +27,59 @@ public class OCTaxZone extends Model {
 
     final Country fromCountry = storeInfo.getLocationAddress().getCountry();
     final Region fromRegion = storeInfo.getLocationAddress().getRegion();
-    
-    
-    String sql = "SELECT " + selectList + ", "
-            + "e.isactive as \"isActive\" "
-            + " FROM C_Tax_Zone e"
-            + "       INNER JOIN C_Tax c_tax_ on e.C_Tax_ID=c_tax_.C_Tax_ID"
-            + " WHERE (e.AD_Client_ID IN :clients)"
-            + "  AND (e.AD_Org_ID IN :orgs)"
-            + "  AND (e.C_Tax_ID IN "
-            + "       (SELECT financialm2_.C_Tax_ID"
-            + "        FROM C_Tax financialm2_"
-            + "        WHERE (financialm2_.SOPOType IN ('S', 'B'))"
-            + "          AND (financialm2_.IsSummary='N'"
-            + "               OR EXISTS (SELECT 1"
-            + "                          FROM C_TaxCategory financialm3_"
-            + "                          WHERE financialm2_.C_TaxCategory_ID=financialm3_.C_TaxCategory_ID"
-            + "                            AND financialm3_.Asbom='Y'))";
+
+    String sql = "SELECT " + selectList + ", " + "e.isactive as \"isActive\" "
+        + " FROM C_Tax_Zone e " + " INNER JOIN C_Tax c_tax_ ON e.C_Tax_ID = c_tax_.C_Tax_ID "
+        + " WHERE (e.AD_Client_ID IN :clients) " + " AND (e.AD_Org_ID IN :orgs) ";
+
+    String sqlTax = " SELECT financialm2_.C_Tax_ID FROM C_Tax financialm2_ "
+        + " WHERE financialm2_.SOPOType IN ('S', 'B') " + " AND (financialm2_.IsSummary = 'N' "
+        + "      OR EXISTS (SELECT 1 FROM C_TaxCategory financialm3_ "
+        + "                 WHERE financialm2_.C_TaxCategory_ID = financialm3_.C_TaxCategory_ID "
+        + "                 AND financialm3_.Asbom = 'Y')) ";
 
     if (fromCountry != null) {
-        sql += "          AND (financialm2_.C_Country_ID=:countryId"
-            + "               OR (financialm2_.C_Country_ID IS NULL)"
-            + "               AND NOT (EXISTS (SELECT 1"
-            + "                                FROM C_Tax_Zone financialm4_"
-            + "                                WHERE financialm4_.C_Tax_ID=financialm2_.C_Tax_ID))"
-            + "               OR EXISTS (SELECT 1"
-            + "                          FROM C_Tax_Zone financialm5_"
-            + "                          WHERE financialm5_.C_Tax_ID=financialm2_.C_Tax_ID"
-            + "                            AND financialm5_.From_Country_ID=:countryId)"
-            + "               OR EXISTS (SELECT 1"
-            + "                          FROM C_Tax_Zone financialm6_"
-            + "                          WHERE financialm6_.C_Tax_ID=financialm2_.C_Tax_ID"
-            + "                            AND (financialm6_.From_Country_ID IS NULL)))";
-    }
-
-    if (fromRegion != null) {
-        sql += "          AND (financialm2_.C_Region_ID=:regionId"
-            + "               OR (financialm2_.C_Region_ID IS NULL)"
-            + "               AND NOT (EXISTS (SELECT 1"
-            + "                                FROM C_Tax_Zone financialm7_"
-            + "                                WHERE financialm7_.C_Tax_ID=financialm2_.C_Tax_ID))"
-            + "               OR EXISTS (SELECT 1"
-            + "                          FROM C_Tax_Zone financialm8_"
-            + "                          WHERE financialm8_.C_Tax_ID=financialm2_.C_Tax_ID"
-            + "                            AND financialm8_.From_Region_ID=:regionId)"
-            + "               OR EXISTS (SELECT 1"
-            + "                          FROM C_Tax_Zone financialm9_"
-            + "                          WHERE financialm9_.C_Tax_ID=financialm2_.C_Tax_ID"
-            + "                            AND (financialm9_.From_Region_ID IS NULL)))";
-    }
-
-    sql += "))";
-
-    if (fromCountry != null) {
-        sql += "  AND (e.From_Country_ID=:countryId OR e.From_Country_ID IS NULL)";
-    }
-    if (fromRegion != null) {
-        sql += "  AND (e.From_Region_ID=:regionId OR e.From_Region_ID IS NULL)";
-    }
-
-    if (jsonParams.optString("lastUpdated", null) != null) {
-        sql += " AND e.updated > :lastUpdated";
+      sqlTax += " AND (financialm2_.C_Country_ID = :countryId "
+          + "      OR (financialm2_.C_Country_ID IS NULL "
+          + "          AND (NOT EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID) "
+          + "               OR EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID AND z.From_Country_ID = :countryId) "
+          + "               OR EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID AND z.From_Country_ID IS NULL)))) ";
     } else {
-        sql += " AND e.IsActive='Y'";
+      sqlTax += " AND financialm2_.C_Country_ID IS NULL ";
     }
 
-    sql += " ORDER BY e.C_Tax_Zone_ID ASC";
-    sql += " LIMIT :limit ";
-    if (offset != 0) {
-        sql += " OFFSET :offset";
+    if (fromRegion != null) {
+      sqlTax += " AND (financialm2_.C_Region_ID = :regionId "
+          + "      OR (financialm2_.C_Region_ID IS NULL "
+          + "          AND (NOT EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID) "
+          + "               OR EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID AND z.From_Region_ID = :regionId) "
+          + "               OR EXISTS (SELECT 1 FROM C_Tax_Zone z WHERE z.C_Tax_ID = financialm2_.C_Tax_ID AND z.From_Region_ID IS NULL)))) ";
+    } else {
+      sqlTax += " AND financialm2_.C_Region_ID IS NULL ";
     }
+
+    sql += " AND e.C_Tax_ID IN (" + sqlTax + ") ";
+    if (POSUtils.filterTaxesFromStoreLocation(posDetail)) {
+      if (fromCountry != null) {
+        sql += " AND (e.From_Country_ID = :countryId OR e.From_Country_ID IS NULL) ";
+      } else {
+        sql += " AND e.From_Country_ID IS NULL ";
+      }
+
+      if (fromRegion != null) {
+        sql += " AND (e.From_Region_ID = :regionId OR e.From_Region_ID IS NULL) ";
+      } else {
+        sql += " AND e.From_Region_ID IS NULL ";
+      }
+    }
+    if (jsonParams.optString("lastUpdated", null) != null) {
+      sql += " AND e.updated > :lastUpdated ";
+    } else {
+      sql += " AND e.IsActive = 'Y' ";
+    }
+
+    sql += " ORDER BY e.C_Tax_Zone_ID ASC ";
+    sql += " LIMIT :limit ";
 
     NativeQuery<?> query = OBDal.getInstance().getSession().createNativeQuery(sql);
     query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
@@ -109,10 +92,10 @@ public class OCTaxZone extends Model {
         .setParameter("limit", limit);
 
     if (fromCountry != null) {
-        query.setParameter("countryId", fromCountry.getId());
+      query.setParameter("countryId", fromCountry.getId());
     }
     if (fromRegion != null) {
-        query.setParameter("regionId", fromRegion.getId());
+      query.setParameter("regionId", fromRegion.getId());
     }
     if (offset != 0) {
       query.setParameter("offset", offset);
